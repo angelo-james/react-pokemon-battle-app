204149edb9f2aaf8523061ca11c80e6026211c9e
AJ Arriola <angelojames.arriola@gmail.com>
2019-05-04 22:49:32 -0700

    imported store and login selection components used react redux to wrap components in a provider and used react router dom to handle routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import store from './store';
+
+import Login from './components/Login';
+import Selection from './components/Selection';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Pokemon Battle App</h1>
-    </div>
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <Route exact path="/" component={ () => <Redirect to="/login" /> } />
+          <Route exact path="/login" component={ Login } />
+          <Route exact path="/selection" component={ Selection } />
+          </div>
+      </Router>
+    </Provider>
   );
 }
 
